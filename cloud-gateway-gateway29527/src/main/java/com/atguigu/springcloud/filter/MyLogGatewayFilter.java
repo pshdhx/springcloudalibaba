aec175b29598f3556891c14ecb5d658f04b25ebf
pshdhx <1799778008@qq.com>
2021-07-11 15:25:25 +0800

    Gateway 自定义过滤器配置类

---
+++ cloud-gateway-gateway29527/src/main/java/com/atguigu/springcloud/filter/MyLogGatewayFilter.java
@@ -0,0 +1,40 @@
+package com.atguigu.springcloud.filter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import java.util.Date;
+/**
+ * @Authtor pshdhx
+ * @Date 2021/7/11 15:23
+ * @Version 1.0
+ */
+
+
+@Component
+@Slf4j
+public class MyLogGatewayFilter implements GlobalFilter,Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        log.info("*********come in MyLogGateWayFilter: "+new Date());
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if(StringUtils.isEmpty(username)){
+            log.info("*****用户名为Null 非法用户,(┬＿┬)");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);//给人家一个回应
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+}
